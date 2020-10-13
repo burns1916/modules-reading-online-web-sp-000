120eaddb294ef153074b317586137895e7d01e04
Ryan K Burns <ryankburns@gmail.com>
2020-10-13 17:59:55 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  include Dance
+
+  attr_accesssor :name
+
+    def initialize(name)
+      @name = name
+    end
+
+end
